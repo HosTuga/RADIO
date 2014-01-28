3337612db4d39f250809b4acb9b7f6320750c74f
HOSTUGA <ninofryzenn@gmail.com>
2014-01-28 03:08:58 +0000

    RADIO PHP CODE FREE TO ALL
    THAT SCRIPT/CODE WAS CREATED BY HOSTTUGA.
    ALL CODE, WE ARE SHARING TO ALL PEOPLE.
    KEEP SHARING.

---
+++ radio.php
@@ -0,0 +1,17 @@
+<script type="text/javascript" src="http://hosted.musesradioplayer.com/mrp.js"></script>
+<script type="text/javascript">
+MRP.insert({
+'url':'http://72.251.240.66:8000/8000',
+'lang':'pt',
+'codec':'mp3',
+'volume':5,
+'autoplay':true,
+'buffering':5,
+'title':'HOSTTUGA - WEBSITE',
+'welcome':'EM DESENVOLVIMENTO',
+'bgcolor':'#FFFFFF',
+'skin':'eastanbul',
+'width':467,
+'height':26
+});
+</script>
